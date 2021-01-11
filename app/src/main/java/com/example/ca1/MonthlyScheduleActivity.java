9a962eafbbc5106fba917149eb539fdf6bde1653
wantonmeeman <tokyo.yu.96@gmail.com>
2021-01-11 21:58:44 +0800

    Finished Styling +bugfixes+ logic for the calendarView

--- app/src/main/java/com/example/ca1/MonthlyScheduleActivity.java
+++ app/src/main/java/com/example/ca1/MonthlyScheduleActivity.java
@@ -15,6 +15,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,6 +40,7 @@ import java.util.Random;
 
 import sun.bob.mcalendarview.MCalendarView;
 import sun.bob.mcalendarview.listeners.OnDateClickListener;
+import sun.bob.mcalendarview.listeners.OnMonthChangeListener;
 import sun.bob.mcalendarview.vo.DateData;
 
 
@@ -63,12 +65,74 @@ public class MonthlyScheduleActivity extends AppCompatActivity implements View.O
         MCalendarView calendarView = (MCalendarView) findViewById(R.id.calendarView); // get the reference of CalendarView
         calendarView.markDate(2020,12,30);
         calendarView.hasTitle(false);
+        calendarView.setOnMonthChangeListener(new OnMonthChangeListener() {
+            @Override
+            public void onMonthChange(int year, int month) {
+                int YearB = cal.get(Calendar.YEAR);
+                int MonthB = cal.get(Calendar.MONTH);
+                int YearA;
+                int MonthA;
+
+                cal.set(Calendar.MONTH,month-1);
+                cal.set(Calendar.YEAR, year);
+
+                YearA = cal.get(Calendar.YEAR);
+                MonthA = cal.get(Calendar.MONTH);
+
+                //This code resolves a bug where using the imgButton(to go from Jan2020 -> dec2019),
+                //then swiping right would not increment the year by 1.
+                //Example:
+                //Using ImgButton jan2020 -> dec2019
+                //Swipe right dec2019 -> jan2019(Supposed to be jan2020)
+                //This supposedly doesnt happen vice versa
+                if(YearA == YearB && MonthB - MonthA == 11){
+                    cal.set(Calendar.YEAR, year+1);
+                }
+                currentMonthYear.setText(dateFormat.format(cal));
+            }
+        });
         calendarView.setOnDateClickListener(new OnDateClickListener() {
             @Override
             public void onDateClick(View view, DateData date) {
                     view.setBackgroundResource(R.drawable.ripple);
             }
         });
+        ImageButton prevMnth = (ImageButton)findViewById(R.id.prevMonth);
+        ImageButton nextMnth = (ImageButton)findViewById(R.id.nextMonth);
+
+        nextMnth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("NextOnClick","true");
+                if(cal.get(Calendar.MONTH) == 11) {
+                    calendarView.travelTo(new DateData(cal.get(Calendar.YEAR)+1, 1, 1));
+                }else{
+                    calendarView.travelTo(new DateData(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+2, 1));
+                }
+            }
+        });
+
+        prevMnth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("PrevOnClick","true");
+                if(cal.get(Calendar.MONTH) == 0) {
+                    calendarView.travelTo(new DateData(cal.get(Calendar.YEAR)-1, 12, 1));
+                }else{
+                    calendarView.travelTo(new DateData(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), 1));
+                }
+            }
+        });
+
+       // RecyclerView myrv = findViewById(R.id.recyclerViewTask);
+
+        //Set Layout, here we set LinearLayout
+       // myrv.setLayoutManager(new LinearLayoutManager(this));
+
+        //TodayTaskRecyclerViewAdapter myAdapter = new TodayTaskRecyclerViewAdapter(this, ArrListAlarm);
+
+        //Set an adapter for the View
+        //myrv.setAdapter(myAdapter);
     }
 
     public void onClick(View v) {//Handle When the Monthly/Today buttons are clicked
